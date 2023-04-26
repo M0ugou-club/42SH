3f96a51ec55aa2983ad495c6e88b81f44aa2decb
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-04-26 10:49:49 +0200

    ADD: library of binary tree

---
+++ LIB/binary_tree/src/create_tree.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2023
+** LIB
+** File description:
+** create_tree.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+
+tree_t *create_node(char *command)
+{
+    tree_t *tree = malloc(sizeof(tree_t));
+
+    if (tree) {
+        tree->command = command;
+        tree->left_tree = NULL;
+        tree->right_tree = NULL;
+    }
+    return (tree);
+}
+
+tree_t *add_node(tree_t *left, tree_t *right, char *command)
+{
+    tree_t *tree = create_node(command);
+
+    if (tree) {
+        if (left && right) {
+            tree->left_tree = left;
+            tree->right_tree = right;
+        }
+    }
+    return (tree);
+}
