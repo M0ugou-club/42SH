bd0f708f16530c5a1a9a2599580274bc8756ec23
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-03 17:58:27 +0200

    ADD: create_obj.c function that add an obj in a tree

---
+++ LIB/binary_tree/AST/create_obj.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** 42SH
+** File description:
+** create_obj.c
+*/
+
+#include <stdlib.h>
+#include "tree.h"
+
+tree_t *create_obj(void (*action) (env_t *env, ast_t *ast, int exec_read, int exec_write))
+{
+    return 0;
+}
