7885421c7e0d9119832f6786ffbbbc87a5baad2a
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-04 15:23:16 +0200

    DEL: delete struct ast_t because we already have this one

--- LIB/binary_tree/AST/create_obj.c
+++ LIB/binary_tree/AST/create_obj.c
@@ -6,9 +6,20 @@
 */
 
 #include <stdlib.h>
+#include "ast.h"
 #include "tree.h"
 
-tree_t *create_obj(void (*action) (env_t *env, ast_t *ast, int exec_read, int exec_write))
+tree_t *create_obj(void (*action) (env_t *env, ast_t *ast, int exec_read, int exec_write), data_type_t enum)
 {
-    return 0;
+    obj_t *obj = NULL;
+    tree_t *node = NULL;
+
+    obj = malloc(sizeof(obj_t));
+    if (obj) {
+        obj->action = action;
+        obj->type = enum;
+        obj->data = NULL;
+        node = create_node(obj);
+    }
+    return node;
 }
