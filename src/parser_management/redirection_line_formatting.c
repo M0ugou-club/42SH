9ee396c1d1c94ab80c35886fcb1cc2469788571d
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-05 13:52:37 +0200

    ADD: parser for the AST

---
+++ src/parser_management/redirection_line_formatting.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2023
+** 42SH
+** File description:
+** redirection_line_formatting
+*/
+
+#include <stdlib.h>
+
+char *get_next_word_index(char *line)
+{
+    int i = 0;
+
+    while (line[i] == ' ') {
+        i++;
+    }
+    while (line[i] != ' ') {
+        i++;
+    }
+    return (i);
+}
+
+static char *get_str_from_part(char *line, int middle_index, int index_to_skip, char *str)
+{
+    str = malloc(sizeof(char) * (strlen(line) - index_to_skip));
+    if (str == NULL) {
+        return (NULL);
+    }
+    memset(str, '\0', (strlen(line) - index_to_skip));
+    strncpy(str, line, middle_index - 1);
+    str[middle_index] = ' ';
+    strcat(str, line[middle_index + 1 + index_to_skip]);
+    return (str);
+}
