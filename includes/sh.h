7120dc4c7514b78b46c8b102cac3851f4a56e4a2
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-07 16:22:10 +0200

    ADD: new prototypes

--- includes/sh.h
+++ includes/sh.h
@@ -9,16 +9,26 @@
     #define SH_H_
 
     #include "env.h"
-    #include "ast.h"
+    #include "tree.h"
 
     /* Function pointer */
-    int exec_or(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_and(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_pipe(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_simple_in(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_double_in(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_command(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_double_out(env_t *env, ast_t *ast, int exec_read, int exec_write);
-    int exec_simple_out(env_t *env, ast_t *ast, int exec_read, int exec_write);
+    int exec_or(env_t *env, tree_t *ast);
+    int exec_and(env_t *env, tree_t *ast);
+    int exec_pipe(env_t *env, tree_t *ast);
+    int exec_simple_in(env_t *env, tree_t *ast);
+    int exec_double_in(env_t *env, tree_t *ast);
+    int exec_command(env_t *env, tree_t *ast);
+    int exec_double_out(env_t *env, tree_t *ast);
+    int exec_simple_out(env_t *env, tree_t *ast);
+
+    /* Function in function pointer */
+    int exec_builtin(env_t *env, char **command_array);
+    int exec_intern(env_t *env, char **command_array);
+    int error_exec(void);
+
+    /* Parser */
+    int get_next_word_index(char *line);
+    char *get_str_from_part(char *line, int middle_index, int index_to_skip,
+        char *str);
 
 #endif /* !SH_H_ */
---
+++ includes/utils.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** 42SH
+** File description:
+** utils
+*/
+
+#ifndef UTILS_H_
+    #define UTILS_H_
+
+    char *replace_char(char *str, char to_find, char to_replace);
+
+#endif /* !UTILS_H_ */
