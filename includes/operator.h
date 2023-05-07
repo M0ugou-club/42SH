4fa1cf045dac61ebac367f2ee0e586810fbfebe9
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-07 16:22:39 +0200

    FIX: modify the struct content

--- includes/operator.h
+++ includes/operator.h
@@ -10,12 +10,14 @@
 
     #include "env.h"
     #include "ast.h"
+    #include "tree.h"
 
     #define INDEX_REDIRECT 4
 
     typedef struct operator_s {
         char *op;
-        void (*action) (env_t *env, ast_t *ast, int exec_read, int exec_write);
+        int (*action) (env_t *env, tree_t *ast);
+        data_type_t type;
     } operator_t;
 
 #endif /* !OPERATOR_H_ */
