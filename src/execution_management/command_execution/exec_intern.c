fe8f0cdd66e686257a503e36a752264b833452b2
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-08 15:04:45 +0200

    ADD: link between function in order to make intern command work

--- src/execution_management/command_execution/exec_intern.c
+++ src/execution_management/command_execution/exec_intern.c
@@ -8,20 +8,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "sh.h"
 #include "ast.h"
 #include "builtin.h"
 #include "env.h"
+#include "utils.h"
 
 int execution(char **command_array, char **my_env)
 {
+    int pid = 0;
+    int wstatus = 0;
+
     if (error_exec() == 1) {
-        exit(1);
+        return (1);
     }
-    if (execve(command_array[COMMAND_INDEX], command_array, my_env) == -1) {
-        exit(1);
+    pid = fork();
+    if (pid == -1) {
+        return (-1);
     }
-    return (0);
+    if (pid != 0) {
+        waitpid(pid, &wstatus, 0);
+    } else {
+        if (execve(command_array[COMMAND_INDEX], command_array, my_env) == -1) {
+            exit(-1);
+        }
+    }
+    return (wstatus);
 }
 
 int get_function(char **path_array, char **command_array)
@@ -30,10 +43,11 @@ int get_function(char **path_array, char **command_array)
 
     for (int i = 0; path_array[i] != NULL; i++) {
         new_command = malloc(sizeof(char) * (strlen(path_array[i]) +
-            strlen(command_array[COMMAND_INDEX]) + 1));
+            strlen(command_array[COMMAND_INDEX]) + strlen("/") + 1));
         if (new_command == NULL)
             return (-1);
         strcpy(new_command, path_array[i]);
+        strcat(new_command, "/");
         strcat(new_command, command_array[COMMAND_INDEX]);
         if (access(new_command, 0) == 0) {
             free(command_array[COMMAND_INDEX]);
@@ -52,7 +66,7 @@ char **get_path_array(env_t *env)
 
     while (tmp != NULL) {
         if (strncmp(tmp->env_line, "PATH=", 5) == 0) {
-            path_array = str_to_word_array(&tmp->env_line[5]);
+            path_array = str_to_word_array(&tmp->env_line[5], ":");
         }
         tmp = tmp->next;
     }
@@ -61,5 +75,17 @@ char **get_path_array(env_t *env)
 
 int exec_intern(env_t *env, char **command_array)
 {
+    char **path_array = NULL;
+    char **my_env = NULL;
+
+    path_array = get_path_array(env);
+    if (path_array == -1)
+        return (-1);
+    get_function(path_array, command_array);
+    my_env = get_env_array(env);
+    if (my_env == NULL) {
+        return (-1);
+    }
+    execution(command_array, my_env);
     return (0);
 }
