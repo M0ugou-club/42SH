a5dbb1c5517d61d0745cd45252c2d2867196d7f6
Yanis Moyon <yanis.moyon@proton.me>
2023-05-08 13:15:51 +0200

    ADD: history init functionality

---
+++ src/optional_management/history_system/history_struct/history_init.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** 42SH
+** File description:
+** history_init
+*/
+
+#include <stdlib.h>
+
+#include "history.h"
+
+hist_t *history_init(void)
+{
+    hist_t *new = malloc(sizeof(hist_t));
+    new->command = NULL;
+    new->next = NULL;
+    return (new);
+}
