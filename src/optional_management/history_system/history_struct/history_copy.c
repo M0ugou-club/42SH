7c37f9707f5b02eadd7bde86f89347833a811990
Yanis Moyon <yanis.moyon@proton.me>
2023-05-08 13:13:58 +0200

    ADD: history copy functionnality

---
+++ src/optional_management/history_system/history_struct/history_copy.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2023
+** 42SH
+** File description:
+** history_copy
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "history.h"
+
+hist_t *history_copy(hist_t *history)
+{
+    hist_t *new = history_init();
+
+    for (hist_t *tmp = history; tmp; tmp = tmp->next) {
+        new->command = my_strdup(tmp->command);
+        new->next = history_init();
+        new = new->next;
+    }
+    return (new);
+}
+
