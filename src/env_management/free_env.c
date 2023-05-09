8674e92659ef1539c0ef2f00171f4d6dd00f39e9
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-09 11:11:50 +0200

    FIX: free ressources

--- src/env_management/free_env.c
+++ src/env_management/free_env.c
@@ -13,7 +13,9 @@ int free_env(env_t *env)
     if (env->next != NULL) {
         free_env(env->next);
     }
-    free(env->env_line);
+    if (env->env_line != NULL) {
+        free(env->env_line);
+    }
     free(env);
     return (0);
 }
