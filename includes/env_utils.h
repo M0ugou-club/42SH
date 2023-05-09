1fbcff5efbf23f46dc588c29c2b70df852aad486
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-09 17:21:00 +0200

    ADD: new prototypes

---
+++ includes/env_utils.h
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2023
+** 42SH
+** File description:
+** env_utils
+*/
+
+#ifndef ENV_UTILS_H_
+    #define ENV_UTILS_H_
+    
+    #include "env.h"
+
+    int get_tab_len(char **tab);
+
+    env_t *find_var_in_env(env_t *env, const char *var);
+
+    char *create_new_line(char *var, char *value);
+
+    void add_line_in_env(const char *new_line, env_t *env);
+
+    env_t *remove_line_in_env(const char *var, env_t *env);
+
+    int get_tab_len(char **tab);
+
+    int my_setenv(env_t *env, char **command_array);
+
+    char *my_getenv(env_t *env, char *to_find);
+
+    void build_setenv_command(char **env, char *var, char *str);
+
+    int print_env(env_t *env, char **command_array);
+
+#endif /* !ENV_UTILS_H_ */
