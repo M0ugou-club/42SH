f2263b0d60127839ab3178ddb8c16cb75bd0b60e
Yanis Moyon <yanis.moyon@proton.me>
2023-05-10 22:48:19 +0200

    ADD: added getters, setter, printer and '!' functions - getters: command, end and size - setter: filler - '!': enum, builtin struct, handlers and builtin functions

--- src/optional_management/history_system/includes/history.h
+++ src/optional_management/history_system/includes/history.h
@@ -7,9 +7,10 @@
 
 #ifndef HISTORY_H
     #define HISTORY_H
+
     typedef struct hist_s {
         const char *command;
-        struct hist_s *next;
+        struct hist_s *next, *prev;
     } hist_t;
 
     extern hist_t *shell_history;
@@ -19,8 +20,40 @@
     void history_add(hist_t *history, const char *command);
     void history_free(hist_t *history);
 
-    void history_print(hist_t *history);
-    const char *history_get(hist_t *history, int index);
+    int history_print(hist_t *history);
+    int history_short_print(hist_t *history, char *command);
+
+    const char *history_get_command(hist_t *history, int index);
+    hist_t *history_get_end(hist_t *history);
+    int history_get_size(hist_t *history);
 
     hist_t *history_copy(hist_t *history);
+
+    void history_filler(hist_t *history, char **command);
+
+    // history shortcuts
+
+    typedef enum hist_short_t {
+        NUMBER,
+        STRING_START,
+        STRING_CONTAINS,
+        PREVIOUS,
+        NONE = -1
+    } hist_short_t;
+
+    typedef struct hist_short_handler_a {
+        hist_short_t type;
+        void (*handler)(hist_t *history, char **command);
+    } hist_short_handler_a;
+
+    hist_short_t history_shortcut_finder(const char *command_line);
+    hist_short_t hist_short_set_type(const char *command);
+    void history_shortcut_handler(hist_t *history,
+                              char **command,
+                              hist_short_t type);
+
+    void history_shortcut_number(hist_t *history, char **command);
+    void history_shortcut_previous(hist_t *history, char **command);
+    void history_shortcut_string_contains(hist_t *history, char **command);
+    void history_shortcut_string_start(hist_t *history, char **command);
 #endif //HISTORY_H
