575884eb111d1d05989e3f33bb657e434f63c4d6
Yanis Moyon <yanis.moyon@proton.me>
2023-05-10 22:51:05 +0200

    ADD: size getter, end of history getter and history_get def changed

--- src/optional_management/history_system/history_struct/history_get.c
+++ src/optional_management/history_system/history_struct/history_get.c
@@ -9,17 +9,43 @@
 
 #include "history.h"
 
-const char *history_get(hist_t *history, int index)
+hist_t *history_get_end(hist_t *history)
 {
     hist_t *tmp = history_copy(history);
 
-    if (index < 0)
+    if (!tmp)
         return (NULL);
-    for (int i = 0; i < index; i++) {
-        if (tmp->next == NULL) {
-            return (NULL);
+    for (; tmp->next; tmp = tmp->next);
+    return (tmp);
+}
+
+int history_get_size(hist_t *history)
+{
+    hist_t *tmp = history_copy(history);
+    int size = 0;
+
+    if (!tmp)
+        return (0);
+    for (; tmp; tmp = tmp->next)
+        size++;
+
+    return (size);
+}
+
+const char *history_get_command(hist_t *history, int index)
+{
+    hist_t *tmp = history_copy(history);
+    int size = history_get_size(history);
+
+    if (index < 0) {
+        for (int i = size; i != index && tmp->next; i--) {
+            tmp = tmp->next;
         }
-        tmp = tmp->next;
+    } else if (index >= 0) {
+        for (int i = 0; i != index && tmp->next; i++) {
+            tmp = tmp->next;
+        }
     }
+
     return (tmp->command);
 }
