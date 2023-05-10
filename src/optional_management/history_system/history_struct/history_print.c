afde7fc0003cee9ec6551d8789aadf2d19447d0f
Yanis Moyon <yanis.moyon@proton.me>
2023-05-10 22:52:08 +0200

    ADD: static functions to print - my_itoa - print_index

--- src/optional_management/history_system/history_struct/history_print.c
+++ src/optional_management/history_system/history_struct/history_print.c
@@ -7,20 +7,49 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "history.h"
 
-void *history_print(hist_t *history)
+static char *my_itoa(int i)
+{
+    int size = 0;
+    int tmp = i;
+    char *str;
+
+    for (; tmp > 0; size++)
+        tmp /= 10;
+    str = malloc(sizeof(char) * (size + 1));
+    if (!str)
+        return (NULL);
+    str[size] = '\0';
+    for (int j = size - 1; j >= 0; j--) {
+        str[j] = i % 10 + '0';
+        i /= 10;
+    }
+    return (str);
+}
+
+static void print_index(int i)
+{
+    char *index = my_itoa(i);
+    write(1, index, strlen(index));
+    free(index);
+}
+
+int history_print(hist_t *history)
 {
     hist_t *tmp = history_copy(history);
-    if (!tmp)
-        return (NULL);
+    if (!tmp) {
+        return (84);
+    }
 
     for (int i = 0; tmp; i++) {
-        my_put_nbr(i);
+        print_index(i);
         write(1, "\t", 1);
-        write(1, tmp->command, my_strlen(tmp->command));
+        write(1, tmp->command, strlen(tmp->command));
         write(1, "\n", 1);
         tmp = tmp->next;
     }
+    return (0);
 }
